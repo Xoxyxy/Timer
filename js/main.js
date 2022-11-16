0b40980752d1bedf108bd21f70af7de22f9787d3
Daniil <dvinakovich@gmail.com>
2022-11-16 20:24:31 +0700

    First commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const newYear = new Date('Jan 1 2023 00:00:00')
+
+  const daysNumb = document.querySelector('.timer__days-numb'),
+    hoursNumb = document.querySelector('.timer__hours-numb'),
+    minNumb = document.querySelector('.timer__min-numb'),
+    secNumb = document.querySelector('.timer__sec-numb')
+
+  const daysText = document.querySelector('.timer__days-text'),
+    hoursText = document.querySelector('.timer__hours-text'),
+    minText = document.querySelector('.timer__min-text'),
+    secText = document.querySelector('.timer__sec-text')
+
+  const declOfNum = (number, text) => {
+    let cases = [2, 0, 1, 1, 1, 2];
+    return text[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+  }
+
+  const addNull = value => value < 10 ? `0${value}` : value
+
+  const timeCount = () => {
+    let nowDate = new Date()
+    let timerLeft = newYear - nowDate
+
+    let days = Math.floor(timerLeft / 1000 / 60 / 60 / 24),
+      hours = Math.floor((timerLeft / 1000 / 60 / 60) % 24),
+      min = Math.floor((timerLeft / 1000 / 60) % 60),
+      sec = Math.floor((timerLeft / 1000) % 60)
+
+    daysNumb.innerHTML = addNull(days)
+    hoursNumb.innerHTML = addNull(hours)
+    minNumb.innerHTML = addNull(min)
+    secNumb.innerHTML = addNull(sec)
+
+    daysText.innerHTML = declOfNum(days, ['День', 'Дня', 'Дней'])
+    hoursText.innerHTML = declOfNum(hours, ['Час', 'Часа', 'Часов'])
+    minText.innerHTML = declOfNum(min, ['Минута', 'Минуты', 'Минут'])
+    secText.innerHTML = declOfNum(sec, ['Секунда', 'Секунды', 'Секунд'])
+  }
+
+  timeCount()
+  setInterval(timeCount, 1000)
+})
+
